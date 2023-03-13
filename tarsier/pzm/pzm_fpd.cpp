0ca437976e5e7704d77f36d9343fcde49d1153e9
mr.li <onground@163.com>
2023-03-13 15:46:12 +0800

    Add PZM support, step 2.0: load dll library. Load ok now.

--- tarsier/pzm/pzm_fpd.cpp
+++ tarsier/pzm/pzm_fpd.cpp
@@ -21,11 +21,36 @@ CPZM_Fpd::CPZM_Fpd(QObject *parent, fpd_model_info_t* model)
         return;
     }
     DIY_LOG(LOG_INFO, QString("Load library %1 success.").arg(model->api_lib_pfn));
+    resolve_lib_functions();
 
     BOOL ret;
-    ret = init_lib();
+    ret = reg_pzm_callbacks();
     if(!ret)
     {}
+}
+
+void CPZM_Fpd::resolve_lib_functions()
+{
+    m_hptr_COM_Init = (Fnt_COM_Init)m_api_lib->resolve(m_hstr_COM_Init);
+    m_hptr_COM_Uninit = (Fnt_COM_Uninit)m_api_lib->resolve(m_hstr_COM_Uninit);
+    m_hptr_COM_SetCfgFilePath = (Fnt_COM_SetCfgFilePath)m_api_lib->resolve(m_hstr_COM_SetCfgFilePath);
+    m_hptr_COM_List = (Fnt_COM_List)m_api_lib->resolve(m_hstr_COM_List);
+    m_hptr_COM_Open = (Fnt_COM_Open)m_api_lib->resolve(m_hstr_COM_Open);
+    m_hptr_COM_Close = (Fnt_COM_Close)m_api_lib->resolve(m_hstr_COM_Close);
+    m_hptr_COM_StartNet = (Fnt_COM_StartNet)m_api_lib->resolve(m_hstr_COM_StartNet);
+    m_hptr_COM_StopNet = (Fnt_COM_StopNet)m_api_lib->resolve(m_hstr_COM_StopNet);
+    m_hptr_COM_RegisterEvCallBack = (Fnt_COM_RegisterEvCallBack)m_api_lib->resolve(m_hstr_COM_RegisterEvCallBack);
+    m_hptr_COM_SetCalibMode = (Fnt_COM_SetCalibMode)m_api_lib->resolve(m_hstr_COM_SetCalibMode);
+    m_hptr_COM_GetFPsn = (Fnt_COM_GetFPsn)m_api_lib->resolve(m_hstr_COM_GetFPsn);
+    m_hptr_COM_GetFPsnEx = (Fnt_COM_GetFPsnEx)m_api_lib->resolve(m_hstr_COM_GetFPsnEx);
+    m_hptr_COM_GetFPCurStatus = (Fnt_COM_GetFPCurStatus)m_api_lib->resolve(m_hstr_COM_GetFPCurStatus);
+    m_hptr_COM_GetFPCurStatusEx = (Fnt_COM_GetFPCurStatusEx)m_api_lib->resolve(m_hstr_COM_GetFPCurStatusEx);
+    m_hptr_COM_GetImageMode = (Fnt_COM_GetImageMode)m_api_lib->resolve(m_hstr_COM_GetImageMode);
+    m_hptr_COM_GetImage = (Fnt_COM_GetImage)m_api_lib->resolve(m_hstr_COM_GetImage);
+    m_hptr_COM_AedAcq = (Fnt_COM_AedAcq)m_api_lib->resolve(m_hstr_COM_AedAcq);
+    m_hptr_COM_AedTrigger = (Fnt_COM_AedTrigger)m_api_lib->resolve(m_hstr_COM_AedTrigger);
+    m_hptr_COM_Stop = (Fnt_COM_Stop)m_api_lib->resolve(m_hstr_COM_Stop);
+    m_hptr_COM_ExposeReq = (Fnt_COM_ExposeReq)m_api_lib->resolve(m_hstr_COM_ExposeReq);
 }
 
 CPZM_Fpd::~CPZM_Fpd()
@@ -46,9 +71,9 @@ CPZM_Fpd::~CPZM_Fpd()
     m_api_lib = nullptr;
 }
 
-BOOL CPZM_Fpd::init_lib()
+BOOL CPZM_Fpd::reg_pzm_callbacks()
 {
-    BOOL ret;
+    BOOL ret = TRUE;
     //COM_SetCfgFilePath(m_model_info->cfg_file_pth.toLatin1().data());
 
     /*
@@ -72,4 +97,5 @@ BOOL CPZM_Fpd::init_lib()
 #endif
 
     //COM_SetCalibMode(IMG_CALIB_GAIN | IMG_CALIB_DEFECT);
+    return TRUE;
 }
--- tarsier/pzm/pzm_fpd.h
+++ tarsier/pzm/pzm_fpd.h
@@ -6,6 +6,48 @@
 #include <QObject>
 #include <QLibrary>
 
+/*handler function type*/
+typedef BOOL (*Fnt_COM_Init)();
+typedef BOOL (*Fnt_COM_Uninit)();
+typedef BOOL (*Fnt_COM_SetCfgFilePath)(CHAR* path);
+typedef BOOL (*Fnt_COM_List)(TComFpList* ptComFpList);
+typedef BOOL (*Fnt_COM_ListAdd)(CHAR* psn);
+typedef BOOL (*Fnt_COM_ListDel)(CHAR* psn);
+typedef BOOL (*Fnt_COM_ListClr)();
+typedef BOOL (*Fnt_COM_Open)(CHAR *psn);
+typedef BOOL (*Fnt_COM_Close)();
+typedef BOOL (*Fnt_COM_StopNet)();
+typedef BOOL (*Fnt_COM_StartNet)();
+typedef BOOL (*Fnt_COM_RegisterEvCallBack)(CHAR nEvent,FP_EVENT_CALLBACK funcallback);
+typedef BOOL (*Fnt_COM_SetPreCalibMode)(CHAR nCalMode);
+typedef CHAR (*Fnt_COM_GetPreCalibMode)();
+typedef BOOL (*Fnt_COM_SetCalibMode)(CHAR nCalMode);
+typedef CHAR (*Fnt_COM_GetCalibMode)();
+typedef BOOL (*Fnt_COM_AedAcq)();
+typedef BOOL (*Fnt_COM_Trigger)();
+typedef BOOL (*Fnt_COM_Prep)();
+typedef BOOL (*Fnt_COM_Acq)();
+typedef BOOL (*Fnt_COM_PrepAcq)();
+typedef BOOL (*Fnt_COM_SetAcq)();
+typedef BOOL (*Fnt_COM_ComAcq)();
+typedef BOOL (*Fnt_COM_ExposeReq)();
+typedef BOOL (*Fnt_COM_AedTrigger)();
+typedef BOOL (*Fnt_COM_Stop)();
+typedef BOOL (*Fnt_COM_Dst)();
+typedef BOOL (*Fnt_COM_Dacq)();
+typedef BOOL (*Fnt_COM_Cbct)();
+typedef BOOL (*Fnt_COM_Dexit)();
+typedef UINT32 (*Fnt_COM_GetNumOffLineImg)();
+typedef BOOL (*Fnt_COM_GetImageMode)(TImageMode* ptImageMode);
+typedef BOOL (*Fnt_COM_GetImageShiftMode)(TImageShiftMode* ptImageShiftMode);
+typedef BOOL (*Fnt_COM_GetImageName)(CHAR* name);
+typedef BOOL (*Fnt_COM_GetImageID)(UINT32 *pimgID);
+typedef BOOL (*Fnt_COM_GetImage)(CHAR* pImageBuff);
+typedef BOOL (*Fnt_COM_GetFPsn)(CHAR* psn);
+typedef BOOL (*Fnt_COM_GetFPsnEx)(CHAR index, CHAR* psn);
+typedef CHAR (*Fnt_COM_GetFPCurStatus)();
+typedef CHAR (*Fnt_COM_GetFPCurStatusEx)(CHAR* psn);
+
 class CPZM_Fpd: public QObject
 {
     Q_OBJECT
@@ -19,10 +61,55 @@ private:
     fpd_model_info_t* m_model_info = nullptr; //this var is set by parent, so do not delete it here.
 
 private:
-    BOOL init_lib();
+    void resolve_lib_functions();
+    BOOL reg_pzm_callbacks();
 
 signals:
     void fpdErrorOccurred(QString errorInfo);
-};
 
+private:
+    /*api function name str*/
+    static constexpr const char* m_hstr_COM_Init = "COM_Init";
+    static constexpr const char* m_hstr_COM_Uninit = "COM_Uninit";
+    static constexpr const char* m_hstr_COM_SetCfgFilePath= "COM_SetCfgFilePath";
+    static constexpr const char* m_hstr_COM_List = "COM_List";
+    static constexpr const char* m_hstr_COM_Open = "COM_Open";
+    static constexpr const char* m_hstr_COM_Close = "COM_Close";
+    static constexpr const char* m_hstr_COM_StartNet = "COM_StartNet";
+    static constexpr const char* m_hstr_COM_StopNet = "COM_StopNet";
+    static constexpr const char* m_hstr_COM_RegisterEvCallBack = "COM_RegisterEvCallBack";
+    static constexpr const char* m_hstr_COM_SetCalibMode = "COM_SetCalibMode";
+    static constexpr const char* m_hstr_COM_GetFPsn = "COM_GetFPsn";
+    static constexpr const char* m_hstr_COM_GetFPsnEx = "COM_GetFPsnEx";
+    static constexpr const char* m_hstr_COM_GetFPCurStatus = "COM_GetFPCurStatus";
+    static constexpr const char* m_hstr_COM_GetFPCurStatusEx = "COM_GetFPCurStatusEx";
+    static constexpr const char* m_hstr_COM_GetImageMode = "COM_GetImageMode";
+    static constexpr const char* m_hstr_COM_GetImage = "COM_GetImage";
+    static constexpr const char* m_hstr_COM_AedAcq = "COM_AedAcq";
+    static constexpr const char* m_hstr_COM_AedTrigger = "COM_AedTrigger";
+    static constexpr const char* m_hstr_COM_Stop = "COM_Stop";
+    static constexpr const char* m_hstr_COM_ExposeReq = "COM_ExposeReq";
+
+    /*api function poiter*/
+    Fnt_COM_Init m_hptr_COM_Init = nullptr;
+    Fnt_COM_Uninit m_hptr_COM_Uninit = nullptr;
+    Fnt_COM_SetCfgFilePath m_hptr_COM_SetCfgFilePath = nullptr;
+    Fnt_COM_List m_hptr_COM_List = nullptr;
+    Fnt_COM_Open m_hptr_COM_Open = nullptr;
+    Fnt_COM_Close m_hptr_COM_Close = nullptr;
+    Fnt_COM_StartNet m_hptr_COM_StartNet = nullptr;
+    Fnt_COM_StopNet m_hptr_COM_StopNet = nullptr;
+    Fnt_COM_RegisterEvCallBack m_hptr_COM_RegisterEvCallBack = nullptr;
+    Fnt_COM_SetCalibMode m_hptr_COM_SetCalibMode = nullptr;
+    Fnt_COM_GetFPsn m_hptr_COM_GetFPsn = nullptr;
+    Fnt_COM_GetFPsnEx m_hptr_COM_GetFPsnEx = nullptr;
+    Fnt_COM_GetFPCurStatus m_hptr_COM_GetFPCurStatus = nullptr;
+    Fnt_COM_GetFPCurStatusEx m_hptr_COM_GetFPCurStatusEx = nullptr;
+    Fnt_COM_GetImageMode m_hptr_COM_GetImageMode = nullptr;
+    Fnt_COM_GetImage m_hptr_COM_GetImage = nullptr;
+    Fnt_COM_AedAcq m_hptr_COM_AedAcq = nullptr;
+    Fnt_COM_AedTrigger m_hptr_COM_AedTrigger = nullptr;
+    Fnt_COM_Stop m_hptr_COM_Stop = nullptr;
+    Fnt_COM_ExposeReq m_hptr_COM_ExposeReq = nullptr;
+};
 #endif // PZM_FPD_H
